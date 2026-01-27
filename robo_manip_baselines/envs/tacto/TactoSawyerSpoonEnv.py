4b2893b2a5cdb4f3e00a448daf0453992cffa12b
Naoki Shibayama <naoki.shibayama@aist.go.jp>
2026-01-27 17:02:54 +0900

    Change shape of spoon handle. Add world_idx feature. Fix world_random_scale feature.

--- robo_manip_baselines/envs/tacto/TactoSawyerSpoonEnv.py
+++ robo_manip_baselines/envs/tacto/TactoSawyerSpoonEnv.py
@@ -36,13 +36,24 @@ class TactoSawyerSpoonEnv(TactoSawyerEnvBase):
             roll=0,
         )
 
+        self.start_obj_pos_offsets = np.array(
+            [
+                [-0.3, 0, 0],
+                [-0.2, 0, 0],
+                [-0.1, 0, 0],
+                [0, 0, 0],
+                [0.1, 0, 0],
+                [0.2, 0, 0],
+            ]
+        )
+
     def setup_task_specific_object(self):
         self.box_scaling = 0.4
         self.spoon = px.Body(
             urdf_path=path.join(
                 path.dirname(__file__), "../assets/tacto/objects/spoon/dummy_spoon.urdf"
             ),
-            base_position=[0.8, 0, 0.2],
+            base_position=[0.7, 0, 0.2],
             base_orientation=[0, -0.5, 0, 1],
             global_scaling=0.2,
         )
@@ -50,21 +61,21 @@ class TactoSawyerSpoonEnv(TactoSawyerEnvBase):
             urdf_path=path.join(
                 path.dirname(__file__), "../assets/tacto/objects/box/white_box.urdf"
             ),
-            base_position=[0.8, 0, 0.15],
+            base_position=[0.8, 0, 0.08],
             global_scaling=self.box_scaling,
         )
         self.green_box = px.Body(
             urdf_path=path.join(
                 path.dirname(__file__), "../assets/tacto/objects/box/green_box.urdf"
             ),
-            base_position=[0.8, -0.5, 0.15],
+            base_position=[0.8, -0.5, 0.08],
             global_scaling=self.box_scaling,
         )
         self.blue_box = px.Body(
             urdf_path=path.join(
                 path.dirname(__file__), "../assets/tacto/objects/box/blue_box.urdf"
             ),
-            base_position=[0.8, 0.5, 0.15],
+            base_position=[0.8, 0.5, 0.08],
             global_scaling=self.box_scaling,
         )
 
@@ -74,9 +85,8 @@ class TactoSawyerSpoonEnv(TactoSawyerEnvBase):
             self.rgb_tactiles.add_body(obj)
 
     def reset_task_specific_object(self):
-        for obj in self.all_task_obj:
+        for obj in self.goal_boxes:
             obj.reset()
-        self.modify_world()
 
     def _get_reward(self):
         (x, y, z), _ = self.spoon.get_base_pose()
@@ -98,14 +108,18 @@ class TactoSawyerSpoonEnv(TactoSawyerEnvBase):
     def modify_world(self, world_idx=None, cumulative_idx=None):
         """Modify simulation world depending on world index."""
         if world_idx is None:
-            world_idx = 0
+            world_idx = cumulative_idx % len(self.start_obj_pos_offsets)
 
         if self.world_random_scale is not None:
             rand_offset = np.random.uniform(
                 low=-1.0 * self.world_random_scale, high=self.world_random_scale, size=3
             )
-            for obj in self.all_task_obj:
-                pos = obj.init_base_position.copy()
-                obj.set_base_pose(pos + rand_offset)
+        else:
+            rand_offset = np.zeros(3)
+
+        for obj in [self.spoon, self.start_box]:
+            pos = obj.init_base_position.copy()
+            pos += self.start_obj_pos_offsets[world_idx] + rand_offset
+            obj.set_base_pose(pos, obj.init_base_orientation)
 
         return world_idx
